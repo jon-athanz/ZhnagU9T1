c64504ea7f3479139dd4a9d75cb4238b9015e98b
Jonathan Zhang <jonathanz28@nycstudents.net>
2023-03-21 08:44:04 -0400

    ZhangU9T1

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Bicycle {
+public class Bicycle extends Vehicle {
+    private int gearCount;
+
+    public Bicycle(String model, int wheels, int gears) {
+        super(model, wheels);
+        gearCount = gears;
+    }
+
+    public int getGearCount() {
+        return gearCount;
+    }
+
+    public static void ringBell() {
+        System.out.println("Ring Ring Ring!!!");
+    }
+
 }
--- src/VehicleRunner.java
+++ src/VehicleRunner.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class VehicleRunner {
+public class VehicleRunner
+{
+    public static void main(String[] args)
+    {
+        // testing all available methods on a Vehicle object
+        Vehicle myVehicle = new Vehicle("Some Vehicle", 6);
+        System.out.println(myVehicle.getName());
+        System.out.println(myVehicle.getWheels());
+        myVehicle.move(10);
+        myVehicle.turn(90);
+        myVehicle.brake(0.25);
+
+        // testing all available methods on a Car object
+        Car myCar = new Car("Honda Civic", 4, "leather");
+        System.out.println(myCar.getName()); // inherited method
+        System.out.println(myCar.getWheels()); // inherited method
+        System.out.println(myCar.getFabric()); // method ONLY available on Car objects
+        myCar.move(30); // inherited method
+        myCar.turn(-90); // inherited method
+        myCar.brake(0.50); // inherited method
+        myCar.honk(); // method ONLY available on Car objects
+        myCar.lockDoors(); // method ONLY available on Car objects
+
+        // ---- WRITE YOUR TEST CODE BELOW! ----
+        // testing all available methods on a Bicycle object
+        Bicycle myBicycle = new Bicycle("Hermes", 4, 25);
+        System.out.println(myBicycle.getName());
+        System.out.println(myBicycle.getWheels());
+        System.out.println(myBicycle.getGearCount());
+        myBicycle.move(40);
+        myBicycle.turn(180);
+        myBicycle.brake(0.76);
+        myBicycle.ringBell();
+    }
 }
